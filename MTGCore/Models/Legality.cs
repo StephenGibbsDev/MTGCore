68ecb37436b52d02d426c52e0f17d9f3e80a89e8
StephenGibbs <stephenbandung@hotmail.co.uk>
2019-10-20 10:51:00 +0100

    added base models

---
+++ MTGCore/Models/Legality.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MTGCore.Models
+{
+    public class Legality
+    {
+        public string format { get; set; }
+        public string legality { get; set; }
+    }
+}

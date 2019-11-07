54922d76ca91d55e74987c2aa80b667aa8cbc835
StephenGibbs <stephenbandung@hotmail.co.uk>
2019-11-07 18:40:57 +0000

    added gitignore to ignore all generated json files

--- src/MTGCore.Proxy/Controllers/HomeController.cs
+++ src/MTGCore.Proxy/Controllers/HomeController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MTGCore.Proxy.Models;
+using System.IO;
 
 namespace MTGCore.Proxy.Controllers
 {
@@ -22,7 +25,7 @@ namespace MTGCore.Proxy.Controllers
 
         [Route("v1/cards/{id}")]
         public async Task<IActionResult> cards(int id)
-        
+
         {
             //https://localhost:44305/card/Details?id=397641
             //get path
@@ -30,42 +33,72 @@ namespace MTGCore.Proxy.Controllers
 
 
             //hash path
+            var hashedPath = CalculateMD5Hash(path);
 
 
             //see if path exists as json object
+
+            var root = Environment.CurrentDirectory;
+            var fullPath = $@"{root}\Data\{hashedPath}.json";
+
+            if (!System.IO.File.Exists(fullPath))
+            {
+                //if not 
+                //call mtg service
+
+                var request = new HttpRequestMessage(HttpMethod.Get,
+                   "https://api.magicthegathering.io" + path);
+
+                var httpClient = _httpClientFactory.CreateClient();
+
+                var mtgResponse = await httpClient.SendAsync(request);
+
+                if (mtgResponse.IsSuccessStatusCode)
+                {
+                    var responseStream = await mtgResponse.Content.ReadAsStringAsync();
+                    //store card here as json
+                    System.IO.File.WriteAllText(fullPath, responseStream);
+
+                    return Content(responseStream);
+                }
+
+
+            }
 
             //if yes
             //serve up object using json contents
 
 
-            //if not 
-            //call mtg service
+
 
             //generate new json with {hash}.json from returned string from webservice
 
 
-            
+
 
             //var uri = new Uri(t1);
             //Request.Url.Segments[2]; //Index of directory2
 
-            var request = new HttpRequestMessage(HttpMethod.Get,
-                "https://api.magicthegathering.io" + path);
 
-
-
-            var httpClient = _httpClientFactory.CreateClient();
-
-            var mtgResponse = await httpClient.SendAsync(request);
-
-            if (mtgResponse.IsSuccessStatusCode)
-            {
-                var responseStream = await mtgResponse.Content.ReadAsStringAsync();
-
-                return Content(responseStream);
-            }
 
             return Content("FAIL");
         }
+
+        public string CalculateMD5Hash(string input)
+        {
+            // step 1, calculate MD5 hash from input
+            MD5 md5 = MD5.Create();
+            byte[] inputBytes = Encoding.ASCII.GetBytes(input);
+            byte[] hash = md5.ComputeHash(inputBytes);
+
+            // step 2, convert byte array to hex string
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+            return sb.ToString();
+        }
+
     }
 }

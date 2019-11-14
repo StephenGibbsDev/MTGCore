2a9b32f594e35f9624ce0f8751793ea6cbc919af
StephenGibbs <stephenbandung@hotmail.co.uk>
2019-11-14 19:36:25 +0000

    added deck controller and index

---
+++ src/MTGCore/Controllers/DeckController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MTGCore.Repository;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MTGCore.Controllers
+{
+    public class DeckController : Controller
+    {
+        private readonly IRepoContext _context;
+
+        public DeckController(IRepoContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var decks = _context.Decks.ToList();
+
+
+            return View(decks);
+        }
+
+        public void AddToDeck(int Id)
+        {
+            
+            
+
+
+        }
+    }
+}

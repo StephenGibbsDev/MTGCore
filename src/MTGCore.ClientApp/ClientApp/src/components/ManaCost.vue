7acc8d31b61fd97eb8f6c12746c879176c3561e4
Connor Downes <cdownes1156@hotmail.com>
2020-01-22 23:45:49 +0000

    ManaConversion Implemented

--- src/MTGCore.ClientApp/ClientApp/src/components/ManaCost.vue
+++ src/MTGCore.ClientApp/ClientApp/src/components/ManaCost.vue
@@ -1,18 +1,21 @@
 <template>
-    <span v-html="convertedManaCost"></span> 
-    <!-- <img src="../assets/images/1.svg" /> -->
+    <div>
+        <span v-for="mana in manaArray" v-bind:key="mana">
+            <img :src="require(`../assets/images/${mana}.svg`)" width="20" height="20" />
+        </span>
+    </div>
 </template>
 
 <script>
     import "bootstrap";
     import "bootstrap/dist/css/bootstrap.min.css";
+    
     export default {
 
         props: ['manaCost'],
         computed: {
-            convertedManaCost: function () {
-                /* TODO: Look at moving images to ClientApp assets to allow use of a relative URL https://localhost:44305/images/$1.svg */
-                return this.manaCost.replace(/{(\w)}/g, '<img src="../assets/images/$1.svg" alt="Mana Symbol" height="20" width="20" />');
+            manaArray: function () {
+                return this.manaCost.split(/{(\w+)}/).filter(m => m);
             }
         }
     };

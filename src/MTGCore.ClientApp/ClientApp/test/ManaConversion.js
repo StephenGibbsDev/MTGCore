c098f66f6a51f3bf0e8ab60fab4b98481eed70db
Connor Downes <cdownes1156@hotmail.com>
2020-01-24 07:36:40 +0000

    Yeet
    Unit tests set up, need to implement them

---
+++ src/MTGCore.ClientApp/ClientApp/test/ManaConversion.js
@@ -0,0 +1,12 @@
+﻿import Vue from 'vue'
+import MyComponent from 'src/components/ManaCost.vue'
+
+var assert = require('assert');
+
+describe('Basic Mocha String Test', function () {
+    it('should return number of charachters in a string', function () {
+        assert.equal("Hello".length, 4);
+    }); it('should return first charachter of the string', function () {
+        assert.equal("Hello".charAt(0), 'H');
+    });
+});

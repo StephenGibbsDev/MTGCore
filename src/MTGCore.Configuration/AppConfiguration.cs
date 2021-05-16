d8ef8378a04004ee65818f56038c2404dc3b0d09
Connor <cdownes1156@hotmail.com>
2021-05-16 16:17:21 +0100

    Configuration file which is currently un-used - might be worth removing

---
+++ src/MTGCore.Configuration/AppConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using MTGCore.Configuration.Interfaces;
+
+namespace MTGCore.Configuration
+{
+    public class AppConfiguration : IAppConfiguration
+    {
+        // TODO(CD): Should use env var
+        public string BaseUrl => "http://localhost:8080/";
+    }
+}

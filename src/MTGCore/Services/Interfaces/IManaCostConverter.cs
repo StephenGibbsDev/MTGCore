0bb0318ebc8eef0bfcbc1e163e9a3544b4a45a4b
Connor <cdownes1156@hotmail.com>
2021-05-16 16:23:53 +0100

    Change ManaConversionService into a ManaCostConverter, which converts a mana cost into an IEnumerable<ManaSymbol>

---
+++ src/MTGCore/Services/Interfaces/IManaCostConverter.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using MTGCore.Dtos.Models;
+
+namespace MTGCore.Services.Interfaces
+{
+    public interface IManaCostConverter
+    {
+        IEnumerable<ManaSymbol> Convert(string manaCost);
+    }
+}
---
+++ src/MTGCore/Services/ManaCostConverter.cs
@@ -0,0 +1,25 @@
+﻿using MTGCore.Services.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+using MTGCore.Dtos.Models;
+
+namespace MTGCore.Services
+{
+    public class ManaCostConverter : IManaCostConverter
+    {
+        private readonly IManaSymbolFactory _manaSymbolFactory;
+        private readonly IManaStringParser _manaStringParser;
+
+        public ManaCostConverter(IManaSymbolFactory manaSymbolFactory, IManaStringParser manaStringParser)
+        {
+            _manaSymbolFactory = manaSymbolFactory;
+            _manaStringParser = manaStringParser;
+        }
+
+        public IEnumerable<ManaSymbol> Convert(string manaCost)
+        {
+            var manaArr = _manaStringParser.Parse(manaCost);
+            return manaArr.Select(_manaSymbolFactory.Build);
+        }
+    }
+}

733b11394dd9af1d61a520b0bb50313b8db150c7
Connor <cdownes1156@hotmail.com>
2021-05-16 16:14:31 +0100

    Add a mana symbol class

---
+++ src/MTGCore.Dtos/MTGCore.Dtos/Models/ManaSymbol.cs
@@ -0,0 +1,16 @@
+﻿using MTGCore.Dtos.Enums;
+
+namespace MTGCore.Dtos.Models
+{
+    public class ManaSymbol
+    {
+        public ManaSymbol(ManaSymbolType type, string imageName)
+        {
+            Type = type;
+            ImageName = imageName;
+        }
+        
+        public ManaSymbolType Type { get; }
+        public string ImageName { get; }
+    }
+}

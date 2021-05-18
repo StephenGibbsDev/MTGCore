d9b2070e892779f9d0a0a512a0d6ec58ee9e28cb
Connor <cdownes1156@hotmail.com>
2021-05-18 20:20:17 +0100

    Add home page with dedicated css file

---
+++ src/MTGCore.ClientApp/ClientApp/src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="home-container" class="fill-height">
+    <div>Yeet</div>
+  </div>
+</template>
+
+<script>
+import '../assets/css/Home.css';
+
+export default {
+  name: 'Home'
+}
+</script>

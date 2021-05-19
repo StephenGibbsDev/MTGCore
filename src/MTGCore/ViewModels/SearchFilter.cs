c8aefd3004045fcb03f19dc3d3f7e50153a54321
StephenGibbsDev <stephenbandung@hotmail.co.uk>
2021-05-19 21:41:13 +0100

    Added missed search filter class

---
+++ src/MTGCore/ViewModels/SearchFilter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace MTGCore.ViewModels
+{
+    public class SearchFilter
+    {
+        public string Type { get; set; }
+        public string ColoursOption { get; set; }
+        public List<string> Colours { get; set; }
+        public string Rarity { get; set; }
+        public string Set { get; set; }
+        public string Price { get; set; }
+    }
+}

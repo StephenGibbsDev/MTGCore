2bb7b40e0e432ec6e0e3a13c2bbb9150ed9bb121
Connor <cdownes1156@hotmail.com>
2021-05-20 23:21:43 +0100

    Note for future self

--- src/MTGCore.Dtos/MTGCore.Dtos/Models/DeckCards.cs
+++ src/MTGCore.Dtos/MTGCore.Dtos/Models/DeckCards.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace MTGCore.Dtos.Models
+﻿namespace MTGCore.Dtos.Models
 {
     public class DeckCards
     {
         public int Id { get; set; }
         public CardDto Card { get; set; }
+        // TODO(CD): int?
         public string CardID { get; set; }
 
         public Deck Deck { get; set; }

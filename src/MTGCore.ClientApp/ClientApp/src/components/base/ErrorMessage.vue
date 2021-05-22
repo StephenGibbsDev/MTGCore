1d2ab3d577f1b4e3865d2c27a26fbd0e23893460
Connor <cdownes1156@hotmail.com>
2021-05-22 20:28:24 +0100

    Extract ErrorMessage and Loader components from Decks view. Some small refactoring the the deck view and styling changes.

---
+++ src/MTGCore.ClientApp/ClientApp/src/components/base/ErrorMessage.vue
@@ -0,0 +1,35 @@
+﻿<template>
+  <div class="alert-container" v-if="visible">
+    <div class="alert alert-danger" role="alert">
+      {{
+        message
+      }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ErrorMessage",
+  props: {
+    message: {
+      type: String,
+      required: false
+    },
+    visible: {
+      type: Boolean,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.alert-container {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  padding: 10px;
+  z-index: 2;
+}
+</style>
--- src/MTGCore.ClientApp/ClientApp/src/views/Decks.vue
+++ src/MTGCore.ClientApp/ClientApp/src/views/Decks.vue
@@ -1,43 +1,42 @@
 <template>
-  <div id="deck-container" class="fill-height">
+  <div id="deck-container" class="fill-height overflow-y-hidden">
     <div style="display: flex; height: 100%;">
-      <div style="border-right: 1px solid #dee2e6; flex-basis: 600px; overflow-x: hidden">
-        <form id="form" v-on:submit.prevent="">
-          <div class="form-group search-container md">
-            <div class="input-group">
-              <input v-model="Name"
-                     v-on:keyup.enter="SubmitForm"
-                     id="card-search"
-                     type="text"
-                     class="form-control"
-                     placeholder="Search for a card"
-                     aria-label="Card Search"
-                     aria-labelledby="card-search-title"/>
-              <div class="input-group-append">
-                <button class="btn btn-default input-group-text"
-                        type="button"
-                        v-on:click="SubmitForm">
-                  Search
-                </button>
+      <div style="border-right: 1px solid #dee2e6; flex-basis: 400px; overflow: hidden; display: flex; flex-direction: column;">
+        <div>
+          <form id="form" v-on:submit.prevent="">
+            <div class="form-group search-container md">
+              <div class="input-group">
+                <input v-model="Name"
+                       v-on:keyup.enter="SubmitForm"
+                       id="card-search"
+                       type="text"
+                       class="form-control"
+                       placeholder="Search for a card"
+                       aria-label="Card Search"
+                       aria-labelledby="card-search-title"/>
+                <div class="input-group-append">
+                  <button class="btn btn-default input-group-text"
+                          type="button"
+                          v-on:click="SubmitForm">
+                    Search
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-          <ResultsTable v-on:addCardToDeck="addToDeck" v-bind:post="searchedCards"/>
-        </form>
-        <div v-if="searchInProgress" style="width: 100%; text-align: center; padding: 20px;">
-          <div class="spinner-border" role="status">
-            <span class="sr-only">Loading...</span>
-          </div>
+          </form>
         </div>
-        <div class="alert alert-danger" role="alert" v-if="searchedCardsLoadingError">{{
-            searchedCardsLoadingError
-          }}
+        <div class="overflow-y-auto" style="height: 100%; position: relative;">
+          <Loader :visible="searchInProgress" :background="'Dark'" />
+          <ErrorMessage :visible="!!searchedCardsLoadingError" :message="searchedCardsLoadingError" />
+          <CardList v-on:addCardToDeck="addToDeck" v-bind:card="searchedCards"/>
         </div>
       </div>
       <div style="flex-grow: 1;">
-        <div style="padding: 10px;">
-          <h3>Cards in Deck</h3>
-          <div>
+        <div class="form-group search-container md" style="display: flex;">
+          <div style="flex-grow: 1;">
+            
+          </div>
+          <div style="width: 300px;">
             <DeckList ref="deckListRef"
                       v-on:triggerChange="updateDeckCardList"
                       v-on:addDeck="addNewDeck"
@@ -53,26 +52,29 @@
   </div>
 </template>
 
-<script>
-import DeckCardList from '@/components/DeckCardList.vue'
-import ResultsTable from "@/components/ResultsTable.vue";
-import DeckList from "@/components/DeckList.vue";
-import axios from "axios";
-
-import '../assets/css/Decks.css';
+<script lang="ts">
+import DeckCardList from '../components/DeckCardList.vue'
+import CardList from "../components/CardList.vue";
+import DeckList from "../components/DeckList.vue";
+import Loader from "../components/base/Loader.vue";
+import ErrorMessage from "../components/base/ErrorMessage.vue";
+import DeckService from "../services/deck";
+import axios from 'axios';
 
 export default {
   name: 'Decks',
   components: {
-    ResultsTable,
+    CardList,
     DeckCardList,
-    DeckList
+    DeckList,
+    Loader,
+    ErrorMessage
   },
   data: function () {
     return {
       Name: "",
       searchedCards: null,
-      searchedCardsLoadingError: "",
+      searchedCardsLoadingError: null,
       searchInProgress: false,
       deckCards: null,
       deckList: null,
@@ -80,39 +82,16 @@ export default {
     };
   },
   methods: {
-    addNewDeck(title) {
-      const params = new URLSearchParams();
-      params.append('title', title);
-
-      axios({
-        method: "post",
-        url: `https://localhost:44305/api/Deck/New`,
-        data: params
-      })
-          .then(res => {
-            this.updateDeckCardList(res.data);
-            this.updateDeckList();
-            // Set selected dropdown value to new deck ID
-            this.selectedDeck = res.data;
-            // Set textbox val back to empty string
-            this.$refs.deckListRef.resetNewDeckName();
-          })
-          .catch(err => {
-            alert(`There was an error submitting your form. See details: ${err}`);
-          });
+    async addNewDeck(title) {
+      const newDeckId = await DeckService.createDeck(title);
+      this.updateDeckCardList(newDeckId);
+      this.updateDeckList();
+      // Set textbox val back to empty string
+      this.$refs.deckListRef.resetNewDeckName();
     },
-    addToDeck(id) {
-      axios({
-        method: "post",
-        url: `https://localhost:44305/api/Deck/Add/${this.selectedDeck}/${id}`,
-        data: this.$data
-      })
-          .then(() => {
-            this.updateDeckCardList(this.selectedDeck);
-          })
-          .catch(err => {
-            alert(`There was an error submitting your form. See details: ${err}`);
-          });
+    async addToDeck(cardId) {
+      await DeckService.addCardToDeck(this.selectedDeck, cardId);
+      this.updateDeckCardList(this.selectedDeck);
     },
     SubmitForm() {
       this.searchInProgress = true;
@@ -133,38 +112,30 @@ export default {
             this.searchInProgress = false;
           });
     },
-    updateDeckCardList(id) {
+    async updateDeckCardList(id) {
+      this.deckCards = await DeckService.getDeckById(id);
       this.selectedDeck = id;
-      axios({
-        method: "get",
-        //todo: make this call based on the selected dropdown ID
-        url: `https://localhost:44305/api/Deck/${id}`,
-        data: this.$data
-      })
-          .then(res => {
-            this.deckCards = res.data;
-          })
-          .catch(err => {
-            alert(`There was an error submitting your form. See details: ${err}`);
-          });
     },
-    updateDeckList() {
-      axios({
-        method: "get",
-        url: "https://localhost:44305/api/Deck",
-        data: this.$data
-      })
-          .then(res => {
-            this.deckList = res.data;
-          })
-          .catch(err => {
-            alert(`There was an error submitting your form. See details: ${err}`);
-          });
+    async updateDeckList() {
+      this.deckList = await DeckService.getDecks();
     }
   },
   mounted() {
-    // this.updateDeckCardList(this.selectedDeck);
-    // this.updateDeckList();
+    this.updateDeckList();
+    if (this.deckList && this.deckList.length > 0) {
+      alert('Hit');
+      this.updateDeckCardList(this.selectedDeck);
+    }
   }
 }
 </script>
+
+<style scoped>
+.search-container.md {
+  margin: 0;
+  padding: 10px;
+  box-shadow: 0px 3px 1px 0px rgb(222, 226, 230);
+  position: relative;
+  z-index: 1;
+}
+</style>

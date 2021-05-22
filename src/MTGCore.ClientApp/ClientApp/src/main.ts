fbf4c43157169498947a538097b74b8c74ccb6b4
Connor <cdownes1156@hotmail.com>
2021-05-22 23:09:47 +0100

    Make the vue router setup file use typescript

--- src/MTGCore.ClientApp/ClientApp/src/main.ts
+++ src/MTGCore.ClientApp/ClientApp/src/main.ts
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router/index'
 import CardService from "./services/card";
 import DeckService from "./services/deck";
 
 Vue.config.productionTip = false
 
-const baseUrl = process.env.VUE_APP_API_BASE_URL ?? '';
+const baseUrl = process.env.VUE_APP_API_BASE_URL;
+if (!baseUrl) throw new Error('Base url is not set-up');
+    
 CardService.init(baseUrl);
 DeckService.init(baseUrl);
 

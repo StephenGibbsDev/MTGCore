a945f21902515f253ff1e015681da972b2e4cc47
Connor <cdownes1156@hotmail.com>
2021-05-23 00:33:25 +0100

    Additional Typescript support

--- src/MTGCore.ClientApp/ClientApp/src/views/Home.vue
+++ src/MTGCore.ClientApp/ClientApp/src/views/Home.vue
@@ -1,17 +1,18 @@
 <template>
-  <div id="home-container" class="fill-height">
-    <div>Yeet</div>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'Home'
-}
-</script>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
-<style scoped>
-#home-container {
-  background-color: goldenrod;
-}
-</style>
+@Component({
+  components: {
+    HelloWorld,
+  },
+})
+export default class Home extends Vue {}
+</script>

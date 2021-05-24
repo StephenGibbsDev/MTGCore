1f8e552698fd838a221b1ad1ef8de851fef4e137
StephenGibbsDev <stephenbandung@hotmail.co.uk>
2021-05-24 22:05:14 +0100

    Added new exception

---
+++ src/MTGCore/Services/Exceptions/SearchFilterMapperException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MTGCore.Services.Exceptions
+{
+    public class SearchFilterMapperException : Exception
+    {
+        public SearchFilterMapperException(string message) : base(message)
+        {
+        }
+    }
+}

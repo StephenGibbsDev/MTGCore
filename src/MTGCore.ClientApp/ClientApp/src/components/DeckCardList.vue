29ff4ea8b7fb7f4aaac942ea800e75aa4786a63f
Connor <cdownes1156@hotmail.com>
2021-05-31 21:00:26 +0100

    Update to display cards with newly updated controller endpoints

--- src/MTGCore.ClientApp/ClientApp/src/components/DeckCardList.vue
+++ src/MTGCore.ClientApp/ClientApp/src/components/DeckCardList.vue
@@ -1,16 +1,61 @@
 <template>
-  <ul v-if="visible">
-    <li v-for="item in deckCards.cards" v-bind:Key="item.card.multiverseid">{{item.amount}} X {{item.card.name}}</li>
-  </ul>
+  <div v-if="visible">
+    <ul class="list-group">
+      <li
+        class="list-group-item d-flex justify-content-between align-items-center"
+        v-for="item in visibleCards"
+        v-bind:Key="item.card.id"
+      >
+        {{ item.card.name }}
+        <span
+          @click="removeCard(item)"
+          class="btn btn-primary"
+          >Remove</span
+        >
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-  props: ["deckCards"],
+  props: ["deck"],
+  data() {
+    return {
+      cardList: this.calculateCards(this.deck),
+    };
+  },
   computed: {
-    visible: function() {
-      return this.deckCards;
-    }
-  }
+    visible: function () {
+      return this.cardList;
+    },
+    visibleCards: function () {
+      return this.cardList.filter((x) => x.deletionRequestSent === false);
+    },
+  },
+  watch: {
+    deck: function (newVal) {
+      this.cardList = this.calculateCards(newVal);
+    },
+  },
+  methods: {
+    removeCard: function (card) {
+      card.deletionRequestSent = true;
+      this.$emit("cardRemoved", card.card.id);
+    },
+    calculateCards: function (cardList) {
+      if (!cardList) {
+        return;
+      }
+      
+      var cards = [];
+      cardList.cards.forEach(function (card) {
+        for (let i = 0; i < card.quantity; i++) {
+          cards.push({ card: card, deletionRequestSent: false });
+        }
+      });
+      return cards;
+    },
+  },
 };
 </script>

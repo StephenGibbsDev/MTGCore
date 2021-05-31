532a4189322a6019f4abd91f49a9b5bafa120996
Connor <cdownes1156@hotmail.com>
2021-05-31 20:44:47 +0100

    Set-up a filter extension to allow us to create reusable, testable filters.

---
+++ src/MTGCore.Repository/Filters/DeckFilters.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq.Expressions;
+using MTGCore.Authentication.Identity;
+using MTGCore.Repository.Models;
+
+namespace MTGCore.Services.Decks
+{
+    public static class DeckFilters
+    {
+        public static Expression<Func<Deck, bool>> FilterByUser(MtgIdentity identity)
+        {
+            return m => m.UserId == identity.Id;
+        }
+        
+        public static Expression<Func<Deck, bool>> FilterByTitle(string title)
+        {
+            return m => m.Title == title;
+        }
+    }
+}

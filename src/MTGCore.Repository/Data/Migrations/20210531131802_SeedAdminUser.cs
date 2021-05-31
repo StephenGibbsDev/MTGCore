eba671ba426b62309ab5fe0e6ea944a31f166690
Connor <cdownes1156@hotmail.com>
2021-05-31 20:38:16 +0100

    Create migrations for the new tables structures after the db drop

--- src/MTGCore.Repository/Data/Migrations/20210531131802_SeedAdminUser.cs
+++ src/MTGCore.Repository/Data/Migrations/20210531131802_SeedAdminUser.cs
@@ -1,15 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
 
-namespace MTGCore.Data.Migrations
+namespace MTGCore.Repository.Data.Migrations
 {
-    public partial class seed_user : Migration
+    public partial class SeedAdminUser : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.InsertData(
                 table: "AspNetUsers",
                 columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "EmailConfirmed", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
-                values: new object[] { "b4280b6a-0613-4cbd-a9e6-f1701e926e73", 0, "ca4c3c06-8b3b-4366-be7a-f130a9c59a58", "test@test.test", true, false, null, "TEST@TEST.TEST", "TESTUSER", null, null, false, "a58a642a-737c-4f36-9068-ba153ef1bd79", false, "TestUser" });
+                values: new object[] { new Guid("b4280b6a-0613-4cbd-a9e6-f1701e926e73"), 0, "8550dc9a-ed46-4cb9-b5af-fa9ad41a85c9", "test@test.test", true, false, null, "TEST@TEST.TEST", "TESTUSER", null, null, false, null, false, "TestUser" });
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
@@ -17,7 +18,7 @@ namespace MTGCore.Data.Migrations
             migrationBuilder.DeleteData(
                 table: "AspNetUsers",
                 keyColumn: "Id",
-                keyValue: "b4280b6a-0613-4cbd-a9e6-f1701e926e73");
+                keyValue: new Guid("b4280b6a-0613-4cbd-a9e6-f1701e926e73"));
         }
     }
 }

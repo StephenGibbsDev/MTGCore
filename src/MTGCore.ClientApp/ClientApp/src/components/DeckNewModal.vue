2a1da14b8f44ed70991af30a5c2103b43ebc82d1
Connor <cdownes1156@hotmail.com>
2021-05-31 20:52:24 +0100

    Update naming conventions and add new field for deck creation

--- src/MTGCore.ClientApp/ClientApp/src/components/DeckNewModal.vue
+++ src/MTGCore.ClientApp/ClientApp/src/components/DeckNewModal.vue
@@ -1,48 +1,57 @@
 ﻿<template>
-    <transition name="modal" v-if="ShowModal">
-        <div class="modal" id="TitleModal" style="display: block; background-color: rgba(0, 0, 0, 0.5); transition: opacity 0.3s ease;">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h4 class="modal-title">Add a new deck</h4>
-                        <button @click="close" type="button" id="btnCloseTitleModal" class="close pull-right">&times;</button>
-                    </div>
-                    <div class="modal-body">
-                        Title
-                        <input type="text" class="form-control" v-model="deckname" />
-                    </div>
-                    <div class="modal-footer">
-                        <button @click="adddeckclick" type="button" class="btn btn-primary">Add</button>
-                        <button @click="close" type="button" class="btn btn-primary">Close</button>
-                    </div>
-                </div>
-            </div>
+  <transition name="modal" v-if="ShowModal">
+    <div class="modal" id="TitleModal"
+         style="display: block; background-color: rgba(0, 0, 0, 0.5); transition: opacity 0.3s ease;">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h4 class="modal-title">Add a new deck</h4>
+            <button @click="close" type="button" id="btnCloseTitleModal" class="close pull-right">&times;</button>
+          </div>
+          <div class="modal-body">
+            Title <span class="small" style="color: indianred;">*</span>
+            <input type="text" class="form-control" v-model="deckName"/>
+            Description
+            <textarea class="form-control" v-model="description"/>
+          </div>
+          <div class="modal-footer">
+            <button :disabled="!canSubmit" @click="addDeckClick" type="button" class="btn btn-primary">Add</button>
+            <button @click="close" type="button" class="btn btn-primary">Close</button>
+          </div>
         </div>
-    </transition>
+      </div>
+    </div>
+  </transition>
 </template>
 
 <script>
-    export default {
-        data: function () {
-            return {
-                ShowModal: false,
-                deckname: ''
-            };
-        },
-        methods: {
-            adddeckclick: function () {
-                // Not sure about emitting the event here, by emitting, it could lead to duplicate code
-                // in each page where this component is used. To abide by the principle of DRY, is 
-                // making the API call in this component better?
-                this.close();
-                this.$emit("addDeckClick", this.deckname);
-            },
-            open: function(){
-                this.ShowModal = true;
-            },
-            close: function(){
-                this.ShowModal = false;
-            }
-        }
+export default {
+  data: function () {
+    return {
+      ShowModal: false,
+      deckName: '',
+      description: ''
     };
+  },
+  computed: {
+    canSubmit: function() {
+      return !!this.deckName;
+    }
+  },
+  methods: {
+    addDeckClick: function () {
+      // Not sure about emitting the event here, by emitting, it could lead to duplicate code
+      // in each page where this component is used. To abide by the principle of DRY, is 
+      // making the API call in this component better?
+      this.close();
+      this.$emit("addDeckClick", { Title: this.deckName, Description: this.description});
+    },
+    open: function () {
+      this.ShowModal = true;
+    },
+    close: function () {
+      this.ShowModal = false;
+    }
+  }
+};
 </script>

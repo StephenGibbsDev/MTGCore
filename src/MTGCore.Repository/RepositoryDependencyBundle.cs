be8b787e979141027e6b17e256289c2e3114b783
Connor <cdownes1156@hotmail.com>
2021-05-31 20:50:50 +0100

    Register deck repository

---
+++ src/MTGCore.Repository/RepositoryDependencyBundle.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MTGCore.Repository.Decks;
+
+namespace MTGCore.Repository
+{
+    public static class RepositoryDependencyBundle
+    {
+        public static void RegisterRepository(this IServiceCollection services)
+        {
+            services.AddScoped<IDeckRepository, DeckRepository>();
+        }
+    }
+}

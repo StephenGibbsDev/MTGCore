2e5f209866eb7e19c87a65d864ba19d32fdd7661
Connor <cdownes1156@hotmail.com>
2021-05-31 20:46:07 +0100

    Add a deck repository to handle db operations for decks

---
+++ src/MTGCore.Repository/Decks/NewDeckData.cs
@@ -0,0 +1,14 @@
+﻿namespace MTGCore.Repository.Decks
+{
+    public class NewDeckData
+    {
+        public NewDeckData(string title, string description)
+        {
+            Title = title;
+            Description = description;
+        }
+        
+        public string Title { get; }
+        public string Description { get; }
+    }
+}

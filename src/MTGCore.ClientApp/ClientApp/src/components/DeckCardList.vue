9f279668be828a2fc45664203dc949aaf27f1da4
Connor <cdownes1156@hotmail.com>
2021-06-02 00:27:18 +0100

    Update how we display cards in the deck

--- src/MTGCore.ClientApp/ClientApp/src/components/DeckCardList.vue
+++ src/MTGCore.ClientApp/ClientApp/src/components/DeckCardList.vue
@@ -1,23 +1,30 @@
 <template>
   <div v-if="visible">
-    <ul class="list-group">
-      <li
-        class="list-group-item d-flex justify-content-between align-items-center"
+    <div class="grid-container">
+      <div
+        class="grid-item"
         v-for="item in visibleCards"
         v-bind:Key="item.card.id"
       >
-        {{ item.card.name }}
-        <span
-          @click="removeCard(item)"
-          class="btn btn-primary"
-          >Remove</span
-        >
-      </li>
-    </ul>
+        <div class="tilt-container" v-tilt>
+          <img :src="item.card.imageUrl" height="310" />
+          <div class="tilt-container-actions">
+            <span @click="removeCard(item)" class="icon fa-stack" title="Remove card from deck" >
+              <i class="fas fa-square fa-stack-2x"></i>
+              <i class="fas fa-times fa-stack-1x fa-inverse"></i>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Vue from "vue";
+import VueTilt from "vue-tilt.js";
+Vue.use(VueTilt);
+
 export default {
   props: ["deck"],
   data() {
@@ -47,8 +54,8 @@ export default {
       if (!cardList) {
         return;
       }
-      
-      var cards = [];
+
+      let cards = [];
       cardList.cards.forEach(function (card) {
         for (let i = 0; i < card.quantity; i++) {
           cards.push({ card: card, deletionRequestSent: false });
@@ -56,6 +63,44 @@ export default {
       });
       return cards;
     },
-  },
+  }
 };
 </script>
+
+<style scoped>
+.grid-container {
+  display: grid;
+  grid-template-columns: auto auto auto auto;
+  background: #eee;
+}
+
+.grid-item {
+  background: #eee;
+  text-align: center;
+  border: 1px solid #e9e9e9;
+}
+
+.tilt-container {
+  padding: 30px 0;
+  transform-style: preserve-3d;
+}
+
+.grid-item:hover {
+  background: #e9e9e9;
+}
+
+.tilt-container-actions {
+  position: absolute;
+  display: none;
+  transform: translateZ(20px);
+  width: 100%;
+}
+
+.grid-item:hover .tilt-container-actions {
+  display: block;
+}
+
+.icon {
+  cursor: pointer;
+}
+</style>
